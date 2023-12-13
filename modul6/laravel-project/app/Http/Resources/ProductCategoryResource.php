2f80d3168543bf01d064227feaa7011dd994f4cf
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2023-12-13 16:00:16 +0700

    437 - Muhammad Alif Nasrulloh

--- modul6/laravel-project/app/Http/Resources/ProductCategoryResource.php
+++ modul6/laravel-project/app/Http/Resources/ProductCategoryResource.php
@@ -15,7 +15,7 @@ class ProductCategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            // "id" => $this->id,
+            "id" => $this->id,
             "name" => $this->name,
             "description" => $this->description
         ];
