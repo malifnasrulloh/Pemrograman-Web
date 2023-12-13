4528cebebb63ac78da73bb24967cbdbea2fa43f4
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2023-12-13 15:27:17 +0700

    kuliah

--- modul6/laravel-project/database/migrations/2023_12_13_075055_create_product_categories_table.php
+++ modul6/laravel-project/database/migrations/2023_12_13_075055_create_product_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("description")->nullable();
             $table->timestamps();
         });
     }
